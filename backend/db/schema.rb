27e11cd8d17e7ccab8d7a31902e9779e9f990e99
StackFlowMaster <acedev709@gmail.com>
2020-10-02 20:25:35 -0400

    adds User resource

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_160356) do
+ActiveRecord::Schema.define(version: 2020_10_03_002306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2020_09_27_160356) do
     t.integer "column"
     t.integer "dozen"
     t.integer "street"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.decimal "balance", precision: 8, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
