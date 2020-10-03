bbb25a07746d594e9e07ee2b227d39f30356d9bc
StackFlowMaster <acedev709@gmail.com>
2020-10-03 16:09:14 -0400

    builds out User model to allow users to sign in using their username

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
         @users = User.all
         render json: UserSerializer.new(@users)
     end
+
+    def create
+        user = User.find_or_create_by(username: params[:username], balance: 1000.00)
+        render json: user, except: [:created_at, :updated_at]
+    end
 end
