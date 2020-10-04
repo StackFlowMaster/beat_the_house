81ba0b57c16c72b5bba488240e314047672324b9
StackFlowMaster <acedev709@gmail.com>
2020-10-04 15:37:37 -0400

    builds out strategy.js Strategy class

---
+++ frontend/src/strategy.js
@@ -0,0 +1,9 @@
+class Strategy {
+    constructor(name, min_balance, starting_bet) {
+        this.name = name;
+        this.minBalance = min_balance;
+        this.startingBet = starting_bet;
+    }
+
+    // method to render object to the DOM
+}
